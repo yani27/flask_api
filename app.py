c5db5513112acc16f5e2965de627a950cace4a3f
yani27 <yanilacamora@gmail.com>
2021-01-18 14:59:44 +0100

    new configuration

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# https://flaskapi-stage.herokuapp.com/ | https://git.heroku.com/flaskapi-stage.git
+# https://flaskapi-pro.herokuapp.com/ | https://git.heroku.com/flaskapi-pro.git
+
 
 @app.route("/")
 def index():
--- config.py
+++ config.py
@@ -1,2 +1,28 @@
-DEBUG = True
-SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
