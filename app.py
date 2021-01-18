a8ed7ec435e54941aafbcaaebcc68ff6ba1b25c0
yani27 <yanilacamora@gmail.com>
2021-01-18 12:04:18 +0100

    first push

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "hi there"
+    return render_template('index.html')
