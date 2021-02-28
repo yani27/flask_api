8d42c9e8accc6125e0031a76a0076750af503b2e
yani27 <yanilacamora@gmail.com>
2021-02-28 15:15:13 +0100

    update to get infos from rapidapi

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+import requests
 
 
 app = Flask(__name__)
@@ -8,10 +9,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/flask_api"
 
-db = SQLAlchemy(app)
-db.create_all()
-from models import User
+# db = SQLAlchemy(app)
+# db.create_all()
+# from models import User
+
 
 @app.route("/")
 def index():
+
+    url = "https://elenasport-io1.p.rapidapi.com/v2/leagues"
+
+    querystring = {"page": "1"}
+
+    headers = {
+        'x-rapidapi-key': "f1627161dbmshd649e45ef5916fcp17cc65jsnebacaaea77f8",
+        'x-rapidapi-host': "elenasport-io1.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+
+    print(response.text)
     return render_template('index.html')
