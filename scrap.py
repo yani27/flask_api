52095958a72bbd15044654ca586d5f43209fd4d7
yani27 <yanilacamora@gmail.com>
2021-03-02 11:51:58 +0100

    add time for each match

--- scrap.py
+++ scrap.py
@@ -21,7 +21,6 @@ soup2 = BeautifulSoup(req2.text, 'html.parser')
 
 container = soup2.findAll("div", {"class": "qa-match-block"})
 
-print(len(container))
 
 for item in range(len(container)):
     print('--| item : {} |--'.format(item))
@@ -29,8 +28,7 @@ for item in range(len(container)):
     league_title = container[item].find(
         "h3", {"class": "gel-minion sp-c-match-list-heading"})
 
-    # print("leaque {} : {}".format(item, league_title.text))
-    # print("adverss : ", adv1.text.encode('utf-8'))
+    print("leaque {} : {}".format(item, league_title.text))
 
     adv = container[item].findAll("span", {
         "class": "gs-u-display-none gs-u-display-block@m qa-full-team-name sp-c-fixture__team-name-trunc"})
@@ -38,8 +36,15 @@ for item in range(len(container)):
     time = container[item].find(
         "span", {"class": "sp-c-fixture__number sp-c-fixture__number--time"})
 
-    print("len in adv = ", len(adv))
-    print("len in tima = ", len(time))
+    k = 0
+    for i in range(len(adv)):
+        print("adv {} : {}".format(i, adv[i].text.encode('utf-8')))
+        k += 1
 
-    # for i in range(len(adv)):
-    #     print("adv {} : {}".format(i, adv[i].text.encode('utf-8')))
+        if k == 2:
+            try:
+                print("in time = {}".format(time.text.encode('utf-8')))
+            except:
+                pass
+            finally:
+                k = 0
